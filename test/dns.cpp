4cde3b2bcfd670471d4a0eb2cf7d3d9e62b10ee9
aurxsiu <xiushou71@qq.com>
2025-05-18 21:35:24 +0800

    稳定了很多,不知道是不是自动配置的dns服务器太烂了,对,北邮的

--- test/dns.cpp
+++ test/dns.cpp
@@ -6,12 +6,12 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
-#pragma comment(lib, "ws2_32.lib")  // 链接 Winsock 库
+#pragma comment(lib, "ws2_32.lib")
 
-#define LOCAL_PORT 53             // 本地监听端口
-#define DNS_SERVER_IP "10.3.9.5"     // 上游 DNS 服务器 IP
-#define DNS_SERVER_PORT 53          // 上游 DNS 端口
-#define BUFFER_SIZE 512             // DNS 数据报最大长度
+#define LOCAL_PORT 53                       // 标准 DNS 端口
+#define DNS_SERVER_IP "114.114.114.114"     // 国内可靠 DNS
+#define DNS_SERVER_PORT 53
+#define BUFFER_SIZE 512
 
 int main() {
     WSADATA wsaData;
@@ -22,14 +22,14 @@ int main() {
 
     // 初始化 Winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsaData)) {
-        fprintf(stderr, "WSAStartup 失败\n");
+        fprintf(stderr, "[!] WSAStartup 失败\n");
         return 1;
     }
 
     // 创建 UDP socket
     sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (sockfd == INVALID_SOCKET) {
-        fprintf(stderr, "socket 创建失败\n");
+        fprintf(stderr, "[!] socket 创建失败\n");
         WSACleanup();
         return 1;
     }
@@ -42,48 +42,47 @@ int main() {
 
     // 绑定本地端口
     if (bind(sockfd, (struct sockaddr*) & localAddr, sizeof(localAddr)) == SOCKET_ERROR) {
-        fprintf(stderr, "绑定端口失败\n");
+        fprintf(stderr, "[!] 绑定端口失败，尝试以管理员身份运行\n");
         closesocket(sockfd);
         WSACleanup();
         return 1;
     }
 
-    printf("DNS 中继服务器启动，监听端口 %d\n", LOCAL_PORT);
+    printf("[*] DNS 中继服务器已启动，监听 UDP 端口 %d\n", LOCAL_PORT);
 
     while (1) {
         // 接收客户端请求
         int recvLen = recvfrom(sockfd, buffer, BUFFER_SIZE, 0,
             (struct sockaddr*) & clientAddr, &addrLen);
         if (recvLen == SOCKET_ERROR) {
-            fprintf(stderr, "接收失败\n");
+            fprintf(stderr, "[!] 接收客户端数据失败\n");
             continue;
         }
-        printf("接收请求");
 
-        // 配置上游 DNS 服务器地址
+        // 配置上游 DNS 地址
         memset(&dnsServerAddr, 0, sizeof(dnsServerAddr));
         dnsServerAddr.sin_family = AF_INET;
         dnsServerAddr.sin_port = htons(DNS_SERVER_PORT);
         dnsServerAddr.sin_addr.s_addr = inet_addr(DNS_SERVER_IP);
 
-        // 将请求转发给上游 DNS
+        // 转发 DNS 请求到上游服务器
         if (sendto(sockfd, buffer, recvLen, 0,
             (struct sockaddr*) & dnsServerAddr, sizeof(dnsServerAddr)) == SOCKET_ERROR) {
-            fprintf(stderr, "转发上游失败\n");
+            fprintf(stderr, "[!] 转发到上游 DNS 失败\n");
             continue;
         }
 
-        // 接收上游 DNS 响应
+        // 接收上游响应
         int respLen = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, NULL, NULL);
         if (respLen == SOCKET_ERROR) {
-            fprintf(stderr, "接收上游响应失败\n");
+            fprintf(stderr, "[!] 接收上游 DNS 响应失败\n");
             continue;
         }
 
-        // 返回给客户端
+        // 返回给原客户端
         if (sendto(sockfd, buffer, respLen, 0,
             (struct sockaddr*) & clientAddr, addrLen) == SOCKET_ERROR) {
-            fprintf(stderr, "返回客户端失败\n");
+            fprintf(stderr, "[!] 返回响应给客户端失败\n");
         }
     }
 
