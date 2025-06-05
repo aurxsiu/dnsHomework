aaf35283abfb322d34d54f58d77e30dbef4f155a
aurxsiu <xiushou71@qq.com>
2025-06-05 22:08:04 +0800

    重新完成了dns的转发,但是似乎出现问题

--- test/dns.cpp
+++ test/dns.cpp
@@ -31,13 +31,15 @@ typedef struct Question
     uint16_t qclass;
 }DNSQuestion;
 
-typedef struct {
+typedef struct ResourceRecord
+{
+    uint16_t name;
     uint16_t type;
-    uint16_t _class;
-    uint32_t ttl;
-    uint16_t rdlength;
-    // 后面是变长 rdata
-} DNSAnswer;
+    uint16_t rrclass;
+    uint16_t ttl;
+    uint16_t rd_length;
+    uint32_t rdata;
+}ResourceRecord;
 #pragma pack(pop)
 
 void print_hex(const unsigned char* data, int len) {
@@ -92,11 +94,42 @@ char* getName(char qname[]) {
     return realName;
 }
 
-//todo
-int* search(char* realName) {
+//todo ip转为10进制
+int search(char* realName) {
     return NULL;
 }
 
+void aurPrint(const char* promt,char* str) {
+    printf("%s: %s\n", promt, str);
+}
+
+int lookup_ip_as_int(char* domain) {
+    /*const char* filename = "";
+    FILE* file = fopen(filename, "r");
+    if (!file) {
+        perror("fopen");
+        return 0;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), file)) {
+        char ip_str[64];
+        char name[256];
+
+        if (line[0] == '#' || line[0] == '\n') continue;
+
+        if (sscanf(line, "%s %s", ip_str, name) == 2) {
+            if (strcmp(name, domain) == 0) {
+                uint32_t ip = inet_addr(ip_str);
+                return ip;
+            }
+        }
+    }
+
+    fclose(file);
+    return -1;*/
+    return -1;
+}
 
 int main() {
     WSADATA wsaData;
@@ -115,7 +148,7 @@ int main() {
 
     memset(&localAddr, 0, sizeof(localAddr));
     localAddr.sin_family = AF_INET;
-    localAddr.sin_addr.s_addr = INADDR_ANY;
+    localAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     localAddr.sin_port = htons(LOCAL_PORT);
 
     if (bind(sockfd, (struct sockaddr*) & localAddr, sizeof(localAddr)) == SOCKET_ERROR) {
@@ -131,79 +164,98 @@ int main() {
     dnsServerAddr.sin_port = htons(DNS_SERVER_PORT);
     dnsServerAddr.sin_addr.s_addr = inet_addr(DNS_SERVER_IP);
 
+
+
     printf("[*] DNS 中继服务器启动，监听端口 %d\n", LOCAL_PORT);
 
     while (1) {
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("\n");
         int recvLen = recvfrom(sockfd, buffer, BUFFER_SIZE, 0,
             (struct sockaddr*) & clientAddr, &addrLen);
         if (recvLen == SOCKET_ERROR) continue;
 
-        //TODO 只从QUERYS字段开始
-        printf("get\n");
-        printf("recvLen: %d \n", recvLen);
-        print_hex((const unsigned char*)buffer, recvLen);
+        if (strcmp("127.0.0.1", inet_ntoa(clientAddr.sin_addr)) == 0) {
+            printf("get message from local\n");
+            printf("recvLen: %d \n", recvLen);
+            print_hex((const unsigned char*)buffer, recvLen);
 
 
 
-        DNSHeader* dns = (DNSHeader*)buffer;
+            DNSHeader* dns = (DNSHeader*)buffer;
 
-        printf("报头id：%u    ", ntohs(dns->id));
-        printf("源ip地址：%s\n", inet_ntoa(clientAddr.sin_addr));
-        
-        DNSQuestion question;
-        uint8_t* offset = (uint8_t*)getquestion(&question, buffer + 12);
+            printf("报头id：%u    ", ntohs(dns->id));
+            printf("源ip地址：%s\n", inet_ntoa(clientAddr.sin_addr));
 
-        printf("name: %s,type: %d,class: %d", question.qname,question.qtype,question.qclass);
+            DNSQuestion question;
+            uint8_t* offset = (uint8_t*)getquestion(&question, buffer + 12);
 
-        //只处理A
-        if (question.qtype == 1) {
             char* name = getName(question.qname);
             printf("realName: %s\n", name);
-            int* searchIp = search(name);
+
+            //只处理A
+            //if (question.qtype == 1) {
+
+            //    int searchIp = lookup_ip_as_int(name);
+
+            //    printf("search_ip: %d", searchIp);
 
 
 
+            //    if (searchIp > 0) {
+            //        printf("使用本地文件返回响应\n");
 
+            //        //flags不管
+            //        dns->ancount = ntohs(1);
 
+            //        static const int A_NAME_OFFSET = 0xC00C;
+            //        static const uint16_t A_TYPE = 1;
+            //        static const uint16_t IN_CLASS = 1;
+            //        static const uint16_t A_RDLENGTH = 4;
+            //        static const uint32_t ttl = 86400;
 
+            //        ResourceRecord answer;
+            //        answer.name = htons(A_NAME_OFFSET);
+            //        answer.type = htons(A_TYPE);
+            //        answer.rrclass = htons(IN_CLASS);
+            //        *(uint32_t*)&answer.ttl = htonl(ttl);
+            //        answer.rd_length = htons(A_RDLENGTH);
+            //        answer.rdata = searchIp;
+            //        memcpy(offset, (uint8_t*)(&answer), 16);
+            //        int new_len = recvLen + 16;
+
+            //        print_hex((const unsigned char*)buffer, new_len);
+
+            //        sendto(sockfd, buffer, new_len, 0, (struct sockaddr*) & clientAddr, addrLen);
+            //        continue;
+            //    }
+            //}
+
+            // 转发到上游 DNS
+            sendto(sockfd, buffer, recvLen, 0,
+                (struct sockaddr*) & dnsServerAddr, sizeof(dnsServerAddr));
+            continue;
         }
+        else {//默认通过114.114.114.114接收响应
+            printf("get message from %s\n", inet_ntoa(clientAddr.sin_addr));
 
-        
+            DNSHeader* resp = (DNSHeader*)buffer;
 
-
-
-
-
-
-
-
-
-
+            printf("[响应] ID=%04x, RCODE=%d, Answer数=%d\n",
+                ntohs(resp->id),
+                ntohs(resp->flags) & 0x000F,
+                ntohs(resp->ancount));
+            print_hex((const unsigned char*)buffer, recvLen);
 
 
-
-
-
-
-        // 转发到上游 DNS
-        sendto(sockfd, buffer, recvLen, 0,
-            (struct sockaddr*) & dnsServerAddr, sizeof(dnsServerAddr));
-
-        // 接收响应
-        int respLen = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, NULL, NULL);
-        if (respLen == SOCKET_ERROR) continue;
-
-        DNSHeader* resp = (DNSHeader*)buffer;
-
-        printf("[响应] ID=%04x, RCODE=%d, Answer数=%d\n",
-            ntohs(resp->id),
-            ntohs(resp->flags) & 0x000F,
-            ntohs(resp->ancount));
-        print_hex((const unsigned char*)buffer, respLen);
-
-
-        // 转发回客户端
-        sendto(sockfd, buffer, respLen, 0, (struct sockaddr*) & clientAddr, addrLen);
+            // 转发回客户端
+            sendto(sockfd, buffer, recvLen, 0, (struct sockaddr*) & localAddr, sizeof(localAddr));
+        }
+       
     }
 
     closesocket(sockfd);
